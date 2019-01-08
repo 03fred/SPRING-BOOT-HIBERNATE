cb9023ddd5fc1c78b412e0a64ec76d629500d8f0
fred <fred.rocke02@gmail.com>
2019-01-08 18:42:39 -0200

    Testando a primeira classe de dominio - Categoria

--- src/main/java/com/fred/cursoomc/resources/CategoriaResource.java
+++ src/main/java/com/fred/cursoomc/resources/CategoriaResource.java
@@ -1,18 +1,27 @@
 package com.fred.cursoomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fred.cursoomc.domain.Categoria;
+
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
-	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		
-		return "REST ESTA FUNCIONANDO";
+
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Categoria> listar() {
+
+		Categoria cat1 = new Categoria(1, "informática");
+		Categoria cat2 = new Categoria(2, "escritório");
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
-	
 
 }
