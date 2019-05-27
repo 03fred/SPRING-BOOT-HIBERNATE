9b007554b2a1ddc9ed643cc9d9886e4e5ce7f0a6
fred <fred@fred-Dell-System-Inspiron-N4110>
2019-05-27 10:03:29 -0300

    validacao sintatica com bean validation

--- src/main/java/com/fred/cursoomc/dto/CategoriaDTO.java
+++ src/main/java/com/fred/cursoomc/dto/CategoriaDTO.java
@@ -2,6 +2,10 @@ package com.fred.cursoomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.fred.cursoomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable {
@@ -11,6 +15,8 @@ public class CategoriaDTO implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	private Integer id;
+	@NotEmpty(message="preenchimento obrigátorio")
+	@Length(min=5,max=80,message="o tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 	
 	public CategoriaDTO() {}
